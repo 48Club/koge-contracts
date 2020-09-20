8d4dccdce351fa47f9958b065869ccf8146b74d3
Michael Zhou <zhoumotongxue008@gmail.com>
2020-09-20 22:00:22 +0800

    Set token name

--- contracts/koge-token/KogeToken.sol
+++ contracts/koge-token/KogeToken.sol
@@ -7,7 +7,7 @@ import "./BEP20Burnable.sol";
 import "./SafeMath.sol";
 
 contract KogeToken is BEP20, BEP20Burnable {
-    constructor() BEP20("KogeToken", "KOGE") {
+    constructor() BEP20("BNB48 Club Token", "KOGE") {
         _mint(msg.sender, 5000000 * (10**uint256(decimals())));
     }
 }
